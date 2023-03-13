9b13e263561cbf9eefd10ba1ea35bccff2caeaae
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-13 14:10:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(rollerskate="roller-skate") {
+        return `This Saturday, I want to ${rollerskate}!`
+    }
+ 
+ saturdayFun()
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+  
